7d7f4675c7baecdbf7d28c1be47a95d81053e340
NoecitoP <noecitopuentes.np@gmail.com>
2017-08-08 10:45:07 -0600

    Add files via upload

---
+++ djangoprojects/MichellStore/Tendencia/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class provider(Models.Model):
+    name=models.CharField(max_length=50,help_text='')
+
+class Product(Models.Model):
+    typeproduct=(
+        ('H','Hombre'),
+        ('M','Mujer'),
+        ('A','Accesorios'),
+        ('Z','Zapatos'),
+        ('C','Cosmeticos')
+        )
+    color=(
+    ('R','Rojo'),
+    ('A','Azul'),
+    ('M','Morado'),
+    ('E','Esmeralda')
+    ('N','Negro')
+    )
+    description=models.CharField(max_length=50,help_text='Ingrese el nombre del producto')
+    provider=models.CharField(max_length=50,help_text='MichellStore')
+    typeproduct=models.CharField(max_length=1,help_text='Seleccione la Categoria del Producto',choices=typeproduct)
+    color=models.CharField(max_length=1,help_text='Seleccione el Color del Producto',choices=color)
+    date=models.DateField(auto_now=True)
+
+class price(Models.Model):
+# Create your models here.
